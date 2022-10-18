0b8b656a718424f5f301b49209b6f514e9c5c968
mongsil0219 <113421366+mongsil0219@users.noreply.github.com>
2022-10-18 16:28:49 +0900

    02

--- main.c
+++ main.c
@@ -3,18 +3,15 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
+int main(void)
+{
+	int i;
+	
+	for(i=0;i<5;i++)
 	{
-		int flag = 1;
-		int y;
-		while(flag != 0)
-		{
-			int y;
-			y = 3;
-			flag = 0;
-		}
-		y = 4;
-		// y 정의X 함수내에서만 실행되고 끝났음
+		int temp = 1;
+		printf("temp = %d\n", temp);
+		temp++;
 	}
 	return 0;
 }
